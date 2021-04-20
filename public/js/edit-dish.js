602e23fd5f4f9e24b1bef9d8568b99e24dbde9a3
Kelsie <kelsie51@hotmail.com>
2021-04-19 23:44:59 -0500

    Fixed update method.

--- public/js/edit-dish.js
+++ public/js/edit-dish.js
@@ -1,14 +1,14 @@
 async function editFormHandler(event) {
   event.preventDefault();
-  const blog_name = document.querySelector('#blog_title').value;
-  const blog_title = document.querySelector('#blog_content').value;
+  const blog_title = document.querySelector('#blog_title').value;
+  const blog_content = document.querySelector('#blog_content').value;
   const user_name = document.querySelector('#user_name').value;
   
   // What will the value of has_nuts be if the box in the form is checked? 
   // The value of has_nuts will be true if the box is checked. 
   // What do we call this kind of operator?
   // We call this a ternary operator. It begins with a condition followed by a question mark and two code blocks separated by a :.
-  const timestamp = document.querySelector('#timestamp:checked') ? true : false;
+  const timestamp = document.querySelector('#timestamp').value;
 
 // window.location gives us access to the URL. We then use the .split() method to access the number at the end of the URL and set that equal to id.
   const id = window.location.toString().split('/')[
@@ -21,8 +21,8 @@ async function editFormHandler(event) {
   const response = await fetch(`/api/blog/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      blog_name,
       blog_title,
+      blog_content,
       user_name,
       timestamp,
     }),
