f457f6c41253e5f349234dc84cb5901b741ea55b
Kelsie <kelsie51@hotmail.com>
2021-04-21 01:05:11 -0500

    Links to individual posts created. Login funtionallity added.

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Blog = require('../../models/Blog');
+const User = require('../../models/User');
 
 // route to create/add a blog
 router.post('/', async (req, res) => {
@@ -64,6 +65,27 @@ router.delete('/:id', async (req, res) => {
 });
 
 
+// CREATE new user
+router.post('/new', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.username = username;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // Login
 router.post('/login', async (req, res) => {
   try {
@@ -91,6 +113,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.loggedIn = true;
+      req.session.username = username;
 
       res
         .status(200)
@@ -99,6 +122,17 @@ router.post('/login', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Blog = require('../models/Blog');
+const User = require('../models/User');
 
 // route to get all dishes
 router.get('/', async (req, res) => {
